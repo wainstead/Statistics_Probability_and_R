7b1c135e7be95da6d79b13d85e8046e1370d64c7
Steve Wainstead <swain@socialserve.com>
2017-04-09 19:52:33 -0400

    Moved class projects directory to the top level

---
+++ course1/lab1/Lesson 2 Learning Path- R Programming for Data Analysts.R
@@ -0,0 +1,67 @@
+x <- c(1,2,3,4,5,6,7,8,9,10)
+x
+class(x)
+x * 3
+x + 2
+x - 10
+x / 4
+x^2
+sqrt(x)
+1:10
+10:-10
+-5:5
+mean(x)
+x
+x <- 1:10
+y <- -5:4
+x+y
+x*y
+x/y
+x^y
+length(x)
+length(y)
+x + c(1,2)
+x + c(1,2,3)
+x <= 5
+x > y
+x <- 10:1
+y <- -4:5
+any(x < y)
+all(x < y)
+q <- c("hockey", "football", "baseball", "soccer")
+q
+q
+q
+nchar(q)
+# everything is a vector
+x
+x[1]
+x[0]
+x[4:6]
+c(One="a", Two="y", Last = "r")
+w <- 1:3
+w
+names(w) <- c("a", "b", "c")
+w
+q2 <- c(q2, "Hockey")
+q2
+q2
+q2Factor <- factor(q2)
+q2Factor
+as.numeric(q2)
+as.numeric(q2Factor)
+x <- 1:10
+mean(x)
+median(x)
+sum(x)
+nchar(x)
+mean(x)
+c(2,6)
+x
+x <- 1:10
+mean(x)
+mean(x, .1)
+x[c(2,6)] <- NA
+x
+sum(x)
+sum(x, na.rm=TRUE)
